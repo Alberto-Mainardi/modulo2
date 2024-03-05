3d021fd117dbf3f2a00e25abb0bd65c80968b448
Alberto-Mainardi <albertomainardi.12@gmail.com>
2024-03-05 19:17:02 -0300

    tarea clase 6

---
+++ clase6/tareaclase6/js/script.js
@@ -0,0 +1,56 @@
+        // Ejercicios While
+
+    // 1
+/*
+let array = [];
+let num = 0;
+while (num < 10){
+    num = num + 1;
+    array[array.length] = num;
+}
+console.log(array);
+*/
+
+    // 2
+/*
+let num = 0;
+let suma = 0;
+while (num < 50) {
+    num = num + 1;
+    suma = suma + num;
+}
+console.log(suma);
+*/
+
+    // 3
+/*
+let num = 2;
+while (num <= 20 && num >= 2){
+    if (num %2 == 0) {
+        console.log(num);
+    }
+    num = num + 1
+}
+*/
+
+
+
+        // Ejercicios Do-While
+
+    // 1
+/*
+let num = 0;
+do{
+    num = num + 1;
+    console.log(num);
+} while (num < 5);
+*/
+
+    // 2
+/*
+let num = 0;
+do{
+    num = Number(prompt("Ingrese un número mayor a 100"));
+} while (num <= 100);
+console.log(num);
+*/
